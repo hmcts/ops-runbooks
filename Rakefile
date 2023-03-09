06ad03fe0091341956e56f24c65f329eca95bd84
Daniel Wilson <daniel.wilson@kainos.com>
2023-03-09 10:53:19 +0000

    init branch

--- Rakefile
+++
@@ -1,4 +0,0 @@
-require 'middleman-gh-pages'
-
-ENV["COMMIT_MESSAGE_SUFFIX"] = "[skip ci]"
-ENV["BRANCH_NAME"] = "ghpages"
--- config.rb
+++
@@ -1,5 +0,0 @@
-require 'govuk_tech_docs'
-GovukTechDocs.configure(self)
-
-set :relative_links, true
-activate :relative_assets
